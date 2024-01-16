f7f70a4296eae114928c845e6894647efff157ac
Alex Bengardt <alexandragorky@yandex.ru>
2024-01-16 12:17:51 +0300

    first commit

---
+++ homework.js
@@ -0,0 +1,24 @@
+let secPerHour = 60 * 60;
+console.log(`В часе ${secPerHour} секунд.`);
+
+let x = 0;
+x += 7;
+console.log(x);
+x *= 4;
+console.log(x);
+x -= 8;
+console.log(x);
+x /= 4;
+console.log(x);
+x **= 3;
+console.log(x);
+x %= 5;
+console.log(x);
+
+console.log(5 > 4); //true
+console.log("ананас" > "яблоко"); //false
+console.log ("2" > "12"); //true
+console.log (undefined == null); //true
+console.log (undefined === null); //false
+console.log (null == "0"); //false
+console.log (null === +"0"); //false
